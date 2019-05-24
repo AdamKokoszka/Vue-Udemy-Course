162cd4c0f2b21846e08316b3ecae954f6a8515bb
Adam <adam.kokoszka.it@gmail.com>
2019-05-25 00:26:20 +0200

    Using Slot

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2 slot="header">Some Header</h2>
+          <p>Any text in paragraph</p>
+          <p>Hello World</p>
+        </app-quote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
-    export default {
-
+  import Quote from "./components/Quote.vue"
+  export default {
+    components: {
+      appQuote: Quote
     }
+  }
+
 </script>
 
 <style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+   <div class="italic">
+    <slot name="header"></slot>
+   </div>
+    <slot></slot>
+  </div>
+  
+</template>
+
+
+<script>
+  export default{
+    props: ["quote"]
+  }
+</script>
+
+<style scoped lang="scss">
+  .container{
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+  h2{
+    color: #fda500;
+  }
+  .italic{
+    font-style: italic;
+    padding: 8px;
+    border: 2px solid #d86c00;
+    border-radius: 10px;
+  }
+</style>
