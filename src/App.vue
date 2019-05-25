8fc07ffd13df60fa1fc2005224cee3620df7a559
Adam <adam.kokoszka.it@gmail.com>
2019-05-26 01:17:24 +0200

    Add file to repository

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,13 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <br>
-        <button @click="cmp = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
-        <button @click="cmp = 'appGreen'" class="btn btn-success">Load Green Template</button>
-        <button @click="cmp = 'appRed'" class="btn btn-danger">Load Red Template</button>
-        <hr>
-        <component :is="cmp">
-          <h3>Hello {{ name }}</h3>
-          <p>Some text</p>
-        </component>
-<!--
-        <app-blue>
-          <h3>Hello {{ name }}</h3>
-          <p>Some text</p>
-        </app-blue>
-        <app-green>
-          <h3>Hello {{ name }}</h3>
-          <p>Some text</p>
-        </app-green>
-        <app-red>
-          <h3>Hello {{ name }}</h3>
-          <p>Some text</p>
-        </app-red>
--->
-      </div>
+    <div class="container">
+      <h1>Hello</h1>
     </div>
-  </div>
 </template>
 
 <script>
-  import Blue from './components/Blue.vue';
-  import Green from './components/Green.vue';
-  import Red from './components/Red.vue';
-
-  export default {
-    data: () => {
-      return {
-        name: "Adam",
-        cmp: "appBlue"
-      };
-    },
-    components: {
-      appBlue: Blue,
-      appGreen: Green,
-      appRed: Red
+    export default {
+        
     }
-  }
-
 </script>
 
 <style>
