d324746396b7c0980186777747cdbf6f06394c62
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-07-09 22:34:59 +0200

    Finish subsection

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 new Vue({
   el: '#app',
