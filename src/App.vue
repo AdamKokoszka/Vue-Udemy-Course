105ec0aa098ee021e3e942dd1c3955585df24ccc
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-07-09 18:22:07 +0200

    Start Project

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,129 @@
 <template>
-  <div class="container">
-    <quote-bar :value_now="quotes.length" :value_max="value_max"></quote-bar>
-    <quote-area @NewQuote="AddQuote"></quote-area>
-    <quote-grid :quotes="quotes" @DeletedItem="DeleteItem"></quote-grid>
-  </div>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                class="form-control">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    value="SendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    value="SendInfoMail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                value="Male"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                value="Female"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control">
+                        <option></option>
+                    </select>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail:</p>
+                        <p>Password:</p>
+                        <p>Age:</p>
+                        <p>Message: </p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li></li>
+                        </ul>
+                        <p>Gender:</p>
+                        <p>Priority:</p>
+                        <p>Switched:</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import QuoteBar from './components/QuoteBar.vue'
-import QuoteArea from './components/QuoteArea.vue'
-import QuoteGrid from './components/QuoteGrid.vue'
-
-export default {
-  data: ()=>{
-    return{
-      quotes:['Some Test'],
-      value_max: 10
-    };
-  },
-  methods:{
-    AddQuote(quote){
-      if( this.quotes.length >= this.value_max ){
-        return alert("Najpierw usuń jakieś notatki!");
-      }
-      this.quotes.push(quote);
-    },
-    DeleteItem(index){
-      if(this.quotes.length <= 1){
-        return alert("Musi być conajmniej jedna notatka!");
-      }
-      this.quotes.splice(index, 1);
+    export default {
     }
-  },
-  components: {
-    QuoteBar,
-    QuoteArea,
-    QuoteGrid
-  }
-}
 </script>
 
-<style lang="scss">
+<style>
 
 </style>
