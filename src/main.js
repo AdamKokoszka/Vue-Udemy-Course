56d645f71041a7bc29f45f8cf77e13b38518513f
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-07-12 23:29:49 +0200

    Finish subsection

--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
+Vue.directive('color', {
+  bind(el, binding, vnode) {
+    //    el.style.backgroundColor = 'green';  
+    //    el.style.backgroundColor = binding.value;
+    let delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg == "bgc") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay)
+  }
+});
 new Vue({
   el: '#app',
   render: h => h(App)
