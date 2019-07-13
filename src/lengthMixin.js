2f127ae728434f6523bab9e9364c60f75ac3176a
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-07-13 13:51:49 +0200

    Start Animations Project

--- src/lengthMixin.js
+++
@@ -1,7 +0,0 @@
-export const lengthMixin = {
-  computed: {
-        calc_length(){
-          return this.text2 + ' (' + this.text2.length + ')';
-        }
-      }
-}
--- src/main.js
+++ src/main.js
@@ -2,11 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-Vue.filter('calc-length', function(value) {
-    return value + ' (' + value.length + ')';
-});
-
-
 new Vue({
   el: '#app',
   render: h => h(App)
