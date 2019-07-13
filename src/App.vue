f128e4af3c01e8221ded3918dde3395a0772fd8c
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-07-13 13:11:54 +0200

    Finish Subsection FiltersMixins

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,38 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
+                <p>{{ text | upperCase | lowercase}}</p>
+                <hr>
+                <button @click="fruits.push('Berries')">Add new item</button>
+                <input type="text" v-model="filterText">
+                <ul>
+                  <li v-for="fruit in filteredFruits">{{ fruit}}</li>
+                </ul>
+                <hr>
+                <list></list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+  import { fruitMixin } from './fruitMixin'
+  import List from './List.vue'
     export default {
+      mixins: [fruitMixin],
+      data(){
+        return{
+          text: 'Some text',
+        }
+      },
+      filters: {
+        upperCase(value){
+          return value.toUpperCase();
+        }
+      },
+      components: {
+        List
+      }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+Vue.filter('lowercase', function(value){
+  return value.toLowerCase();
+});
+Vue.mixin({
+  created(){
+    console.log('Global Mixin - Created Hook');
+  }
+});
 new Vue({
   el: '#app',
   render: h => h(App)
