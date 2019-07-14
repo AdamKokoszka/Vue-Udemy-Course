94462743c0d4309b47d921fcbcf94b5d959715f7
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-07-14 17:14:59 +0200

    In progress

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <button class="btn btn-primary" @click="show = !show">Show alert!</button>
+        <br>
+        <br>
+        <transition>
+          <div class="alert alert-info" v-if="show">Some info</div>
+        </transition>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-
-            }
-        }
+  export default {
+    data() {
+      return {
+        show: false
+      }
     }
+  }
+
 </script>
 
 <style>
