9821edccae91319063487b6d515b071a33eda73e
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-07-15 15:42:15 +0200

    Start Vue-routing subsection

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Animations</h1>
-        <button class="btn btn-primary" @click="show = !show">Show alert!</button>
-        <br>
-        <br>
-        <transition>
-          <div class="alert alert-info" v-if="show">Some info</div>
-        </transition>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        show: false
-      }
+    export default {
     }
-  }
-
 </script>
 
 <style>
-
 </style>
